7a89cf64578112e931acab9f0efcfaf6ef18429a
loretorres <36246967+loretorres@users.noreply.github.com>
2018-02-09 22:19:35 -0300

    Add files via upload

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -4,5 +4,12 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function MostrarDescuento()
 {
-	
+    var Importe
+    var Descuento
+    var NuevoImporte
+
+    Importe=parseInt(document.getElementById("importe").value);
+    Descuento=Importe*0.25;
+    NuevoImporte=Importe-Descuento;
+    document.getElementById("resultado").value=NuevoImporte;
 }
--- 1-EntradaSalida/jsEntradaSalida-8.js
+++ 1-EntradaSalida/jsEntradaSalida-8.js
@@ -4,5 +4,11 @@ mostrar el resto entre el dividendo y el divisor.
 ej.: "El resto es 0 ."*/
 function SacarResto()
 {
-	
+    var Num1;
+    var Num2;
+    var resultado;
+    Num1=parseInt(document.getElementById("numeroDividendo").value);
+    Num2=parseInt(document.getElementById("numeroDivisor").value);
+    resultado=Num1%Num2;
+    alert("el resto es "+resultado);    
 }
